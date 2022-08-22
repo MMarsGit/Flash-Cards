5a1f6d65ab67a0f64a93287d8eda197a6c2eb834
CodingRat <matt.marsland1@gmail.com>
2022-08-22 16:29:26 +0100

    Added function to scrape for translations

--- Classes/Core/Scrape.py
+++ Classes/Core/Scrape.py
@@ -1,12 +1,27 @@
+from asyncio.windows_events import NULL
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-res = requests.get('https://bab.la', headers=headers)
-print(res.text)
-print(res.status_code)
+def search_PolishEng(phrase):
+    #Headers to pass verification tests
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    
+    baseURL = "https://en.bab.la/dictionary/polish-english/"
+    searchURL = baseURL + phrase
 
-#find input
+    page = requests.get(searchURL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    uls = soup.find("ul", {"class", "sense-group-results"})
 
+    lis = uls.find_all("li")
+    items = []
 
-def search_site():
+    for element in lis:
+            a = element.find_all("a")
+            for item in a:
+                items.append(item.text.strip())
+    
+    items.pop(0)
+    return items
+
+search_PolishEng("dzień")
